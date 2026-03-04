190f7f3c681538b22d4106b737666ab4ce2af329
Seth Johnston <sjohnston@seths-macbook.local>
2026-03-04 10:36:05 -0500

    skeleton script.js + css from lab

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
+// set up dimens. and margins
+const margin = { top: 80, right: 60, bottom: 60, left: 100 };
+const width = 800 - margin.left - margin.right;
+const height = 600 - margin.top - margin.bottom;
 
+// create SVG
+const svg = d3.select('#vis')
+    .append('svg')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+    .attr('transform', `translate(${margin.left},${margin.top})`);
