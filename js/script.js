bbc68dae5507210a7e29cbbb214ba1c7a3184662
Seth Johnston <sjohnston@seths-macbook.local>
2026-03-04 11:30:14 -0500

    loaded csv, data in devtools

--- js/script.js
+++ js/script.js
@@ -3,6 +3,9 @@ const margin = { top: 80, right: 60, bottom: 60, left: 100 };
 const width = 800 - margin.left - margin.right;
 const height = 600 - margin.top - margin.bottom;
 
+// global vars. 
+let allData;
+
 // create SVG
 const svg = d3.select('#vis')
     .append('svg')
@@ -10,3 +13,24 @@ const svg = d3.select('#vis')
     .attr('height', height + margin.top + margin.bottom)
     .append('g')
     .attr('transform', `translate(${margin.left},${margin.top})`);
+
+// load csv and transform data
+function init() {
+    d3.csv("./data/east_coast_weather.csv",
+    function(d) {
+        return {
+            state: d.state,
+            week: +d.week,
+            snwd: +d.SNWD,
+            tavg: +d.TAVG,
+            snwf: +d.SNOW
+        }
+    })
+    .then(data => {
+        console.log(data)
+        allData = data
+    })
+    .catch(error => console.error('Error loading data:', error));
+}
+
+init();
